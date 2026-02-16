976a41000088d22ee41aa638b0f2d7a06428d47b
LAPTOP-V5IC4G6U\Adil Talukdar <adil.talukdar12j@gmail.com>
2026-02-16 21:31:18 +0530

    User service working

---
+++ userservice/src/main/java/com/fitness/userservice/model/UserRole.java
@@ -0,0 +1,10 @@
+package com.fitness.userservice.model;
+
+//created ENUM
+//Enum is a special data type used to define a fixed set
+// of constants. It improves type safety by restricting
+// a variable to predefined values.
+
+public enum UserRole {
+    USER, ADMIN
+}

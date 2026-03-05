cefad47657f53497dcd992fc9c1c6021d39af44f
LAPTOP-V5IC4G6U\Adil Talukdar <adil.talukdar12j@gmail.com>
2026-03-05 23:51:14 +0530

    Generating AI Recommendation

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
@@ -0,0 +1,65 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityAIService {
+    private final GeminiService geminiService;
+
+    public String generateRecommendation(Activity activity){
+        String prompt = createPromptForActivity(activity);
+        String aiResponse = geminiService.getAnswer(prompt);
+        log.info("RESPONSE FROM AI: {}", aiResponse);
+        return aiResponse;
+    }
+
+    private String createPromptForActivity(Activity activity) {
+        return String.format("""
+                Analyze this fitness activity and provide detailed recommendation in the following EXACT JSON format:
+                {
+                    "analysis": {
+                        "overall": "Overall analysis here",
+                        "pace": "Pace analysis here",
+                        "heartRate": "Heart rate analysis here",
+                        "caloriesBurned": "Calories analysis here"
+                    },
+                    "improvement": [
+                        {
+                            "area": "Area name",
+                            "recommendation": "Detailed recommendation"
+                        }
+                    ],
+                    "suggestion": [
+                        {
+                            "workout": "Workout name",
+                            "description": "Detailed workout description"
+                        }
+                    ],
+                    "safety": [
+                        "Safety point 1",
+                        "Safety point 2"
+                    ]
+                }
+                
+                Analyze this activity:
+                Activity Type: %s
+                Duration: %d minutes
+                Calories Burned: %d
+                Additional Metrics: %s
+                
+                provide detailed analysis focusing on performance, improvements, next workout suggestion, and safety guielines.
+                Ensure the response follows the EXACT JSON format shown above.
+                """,
+
+                activity.getType(),
+                activity.getDuration(),
+                activity.getCaloriesBurned(),
+                activity.getAdditionalMetrics()
+        );
+    }
+}
